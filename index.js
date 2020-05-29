d0e898856838d35c325d74f078f8d11af155534e
dzarrr <dzarbelahanifa@gmail.com>
2020-05-29 12:11:12 +0700

    add solution for exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('combined'))
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
