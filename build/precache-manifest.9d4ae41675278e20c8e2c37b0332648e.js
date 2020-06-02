fae943325eb3909963a298016be7f742a70c4ac1
dzarrr <dzarbelahanifa@gmail.com>
2020-06-03 05:40:23 +0700

    add frontend build

---
+++ build/precache-manifest.9d4ae41675278e20c8e2c37b0332648e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7290556037adba66882eb9d7fde9ec2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bea201e942024941f5",
+    "url": "/static/js/main.9cdc2493.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
